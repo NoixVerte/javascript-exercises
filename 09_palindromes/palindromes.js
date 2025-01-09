944d6546e802b97cbf23464cd96f4de1d0750ce5
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-09 13:16:45 +0200

    Implement function to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    for (let i  = 0; i < newString.length; i++) {
+        if (newString[i] != newString[newString.length - i - 1]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
