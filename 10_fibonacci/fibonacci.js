c8ea24964b9276c219311cd4bc38e8d2b35fe23a
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-09 13:55:31 +0200

    Implement function to give specific number in a fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    let currentNumber = 1;
+    let previousNumber = 0;
+    for (let i = 1; i < num; i++) {
+        currentNumber += previousNumber;
+        previousNumber = currentNumber - previousNumber;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
