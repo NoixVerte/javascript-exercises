c8d53ef3301ab54161c860ddb75d99972a6ba921
NoixVerte <65561735+NoixVerte@users.noreply.github.com>
2025-01-10 09:43:52 +0200

    Implement function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPersonAge = 0;
+    let oldesPersonIndex;
+    const currentYear = new Date().getFullYear(); 
+    people.forEach(person => {
+        person.yearOfDeath = person.yearOfDeath ?? currentYear;
+        if (person.yearOfDeath - person.yearOfBirth > oldestPersonAge) {
+            oldestPersonAge = person.yearOfDeath - person.yearOfBirth;
+            oldesPersonIndex = people.indexOf(person);
+        }
+    });
+    return people[oldesPersonIndex];
 };
 
 // Do not edit below this line
